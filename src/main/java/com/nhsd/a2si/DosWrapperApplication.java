afdf2ec3b466bcc4184a850073c5189df09fa88b
Daniel Dammann <marcdanieldammann@gmail.com>
2018-08-14 16:48:22 +0100

    Got rid of ComponentScan import in DosWrapperApplication again

--- src/main/java/com/nhsd/a2si/DosWrapperApplication.java
+++ src/main/java/com/nhsd/a2si/DosWrapperApplication.java
@@ -2,7 +2,6 @@ package com.nhsd.a2si;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages={"com.nhsd.a2si.client.dos"})
 public class DosWrapperApplication {
