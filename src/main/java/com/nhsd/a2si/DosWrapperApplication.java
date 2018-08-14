51bc909c39deecde4fb1b3529923a5dd07a3ed65
Daniel Dammann <marcdanieldammann@gmail.com>
2018-08-14 15:51:51 +0100

    Added to spring bootstrap @SpringBootApplication(scanBasePackages={"com.nhsd.a2si.client.dos"})

--- src/main/java/com/nhsd/a2si/DosWrapperApplication.java
+++ src/main/java/com/nhsd/a2si/DosWrapperApplication.java
@@ -2,8 +2,9 @@ package com.nhsd.a2si;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.nhsd.a2si.client.dos"})
 public class DosWrapperApplication {
 
 	public static void main(String[] args) {
